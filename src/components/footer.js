1cd461d91dc78c50375d003e8860fc9a48e0231d
Bea Malsky <beamalsky@datamade.us>
2021-03-10 22:54:50 -0600

    Footer tweak

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,7 +19,7 @@ const Footer = () => (
         <em>Market Box is a project from the organizations at 6100 S. Blackstone Ave: <a href="https://invisible.institute/">Invisible Institute</a>, <a href="https://experimentalstation.org/">Experimental Station</a>, <a href="https://southsideweekly.com/">South Side Weekly</a>, and <a href="https://buildcoffee.org/">Build Coffee</a>.</em><br />
         <em>We partner with Middlebrow and Local Foods to source high quality groceries from across the Midwest.</em>
       </p>
-      <p><em>Photos by Davon Clark, Anwuli Anigbo, and Hannah Nyhart. Website by Bea Malsky.</em></p>
+      <p><em>Photos by Davon Clark, Anwuli Anigbo, and Hannah Nyhart</em></p>
     </div>
   </footer>
 )
