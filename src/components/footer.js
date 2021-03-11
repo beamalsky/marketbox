e4540c06a08e9640485eb9ff655b9bd8f5b85ecf
Bea Malsky <beamalsky@datamade.us>
2021-03-10 21:00:17 -0600

    Footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,11 +11,13 @@ const Footer = () => (
       style={{
         margin: `0 auto`,
         padding: `1.45rem 1.0875rem`,
-        maxWidth: '1200px'
+        maxWidth: '1200px',
+        textAlign: `center`,
       }}
     >
       <p>
-        <em>Market Box is a project from the organizations at 6100 S. Blackstone Ave:  Invisible Institute, Experimental Station, South Side Weekly, and Build Coffee. We partner with Middlebrow and Local Foods to source high quality groceries from across the Midwest.</em>
+        <em>Market Box is a project from the organizations at 6100 S. Blackstone Ave: <a href="https://invisible.institute/">Invisible Institute</a>, <a href="https://experimentalstation.org/">Experimental Station</a>, <a href="https://southsideweekly.com/">South Side Weekly</a>, and <a href="https://buildcoffee.org/">Build Coffee</a>.</em><br />
+        <em>We partner with Middlebrow and Local Foods to source high quality groceries from across the Midwest.</em>
       </p>
     </div>
   </footer>
