afdb8296941061d4c818c7ba1ed2e29cbb857b20
Bea Malsky <beamalsky@datamade.us>
2021-03-10 21:40:24 -0600

    Add nav items

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Row, Col } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -29,6 +30,17 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
       <h4>is a mutual aid project that sources food from small farms and distributes it for free across the South Side of Chicago</h4>
+      <Row className="justify-content-md-center my-3">
+        <Col xs lg="2">
+          <Link>Home</Link>
+        </Col>
+        <Col xs lg="2">
+          <Link>About</Link>
+        </Col>
+        <Col xs lg="2">
+          <Link>Join Us</Link>
+        </Col>
+      </Row>
     </div>
   </header>
 )
